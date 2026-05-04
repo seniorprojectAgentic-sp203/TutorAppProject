42741d0cb3da9f6cc1e05d3c3eb793f3120c2e07
Ian-T8 <itheriault1000@gmail.com>
2026-05-04 00:02:57 -0400

    changes

--- Frontend/tutor-app/src/firebase/firebaseConfig.js
+++ Frontend/tutor-app/src/firebase/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 //Firebase configuration
 const firebaseConfig = {
@@ -15,6 +16,6 @@ const firebaseConfig = {
 //Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const googleProvider = new  GoogleAuthProvider();
+const db = getFirestore(app);
 
-export {app, auth, googleProvider};
+export {app, auth, db}
